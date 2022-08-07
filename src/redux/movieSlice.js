1ad99b235d971e88225e9452739743aac3c69f5b
mishratapas <mishratapas769@gmail.com>
2022-08-07 19:10:58 +0530

    apikey edited

--- src/redux/movieSlice.js
+++ src/redux/movieSlice.js
@@ -1,70 +1,71 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
 
-import MovieApi from '../common/Api/MovieApi'
-import { MovieApiKey } from '../common/Api/MovieApiKey'
+import MovieApi from "../common/Api/MovieApi";
+import {MovieApiKey} from "../common/Api/MovieApiKey";
 
 export const getMovies = createAsyncThunk(
-  'movies/getMovies',
+  "movies/getMovies",
   async (values) => {
     // const MovieText = 'Harry Potter'
     const response = await MovieApi.get(
-      `?apikey=${MovieApiKey}&s=${values}&type=movie`
-    )
-    return response.data
+      // `?apikey=${MovieApiKey}&s=${values}&type=movie`
+      `?apikey=8fc48f54&s=${values}&type=movie`
+    );
+    return response.data;
   }
-)
+);
 
-export const getShows = createAsyncThunk('shows/getShows', async (values) => {
+export const getShows = createAsyncThunk("shows/getShows", async (values) => {
   // const showText = 'Office'
   const response = await MovieApi.get(
     `?apikey=${MovieApiKey}&s=${values}&type=series`
-  )
-  return response.data
-})
+  );
+  return response.data;
+});
 
-export const getDetail = createAsyncThunk('detail/getDetail', async (id) => {
+export const getDetail = createAsyncThunk("detail/getDetail", async (id) => {
   const response = await MovieApi.get(
     `?apikey=${MovieApiKey}&i=${id}&Plot=full`
-  )
-  return response.data
-})
+  );
+  return response.data;
+});
 
-const initialState = { movies: {}, shows: {}, detail: {}, loading: false }
+const initialState = {movies: {}, shows: {}, detail: {}, loading: false};
 
 const movieSlice = createSlice({
-  name: 'movies',
+  name: "movies",
   initialState,
   reducers: {
     removeThings: (state) => {
-      state.detail = {}
+      state.detail = {};
     },
   },
   extraReducers: {
     [getMovies.pending]: (state, action) => {
-      state.loading = true
+      state.loading = true;
     },
     [getMovies.fulfilled]: (state, action) => {
-      state.loading = false
-      console.log('Movies are fetched')
-      state.movies = action.payload
+      state.loading = false;
+      console.log("Movies are fetched");
+      state.movies = action.payload;
     },
     [getShows.pending]: (state, action) => {
-      state.loading = true
+      state.loading = true;
     },
     [getShows.fulfilled]: (state, action) => {
-      state.loading = false
-      console.log(`Shows are fetched`)
-      state.shows = action.payload
+      state.loading = false;
+      console.log(`Shows are fetched`);
+      state.shows = action.payload;
     },
     [getDetail.pending]: (state, action) => {
-      state.loading = true
+      state.loading = true;
     },
     [getDetail.fulfilled]: (state, action) => {
-      state.loading = false
-      console.log(`details are fetched`)
-      state.detail = action.payload
+      state.loading = false;
+      console.log(`details are fetched`);
+      state.detail = action.payload;
     },
   },
-})
-export const { removeThings } = movieSlice.actions
-export default movieSlice.reducer
+});
+export const {removeThings} = movieSlice.actions;
+export default movieSlice.reducer;
